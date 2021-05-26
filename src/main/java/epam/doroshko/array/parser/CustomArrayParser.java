4b11182a8a755aa2d92c9692fce569acf76c55a5
Violetta Doroshko <violet.doroshko@gmail.com>
2021-05-26 09:03:59 +0300

    fix parser

--- src/main/java/epam/doroshko/array/parser/CustomArrayParser.java
+++ src/main/java/epam/doroshko/array/parser/CustomArrayParser.java
@@ -4,16 +4,14 @@ import epam.doroshko.array.entity.CustomArray;
 
 public class CustomArrayParser {
   public CustomArray parse(String str) {
-/*
-    final var space = "//s";
-    int length = str.split(space).length;
+
+    int length = str.split(" ").length;
     var array = new int[length];
     var i = 0;
-    for (String element : str.split(space)) {
+    for (String element : str.split(" ")) {
       array[i] = Integer.parseInt(element);
       ++i;
     }
-    return new CustomArray(array);*/
-    return null;
+    return new CustomArray(array);
   }
 }
