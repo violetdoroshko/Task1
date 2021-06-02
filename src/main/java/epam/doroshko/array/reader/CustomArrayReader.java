827182e94365567c84dbfc9847fbdecd3c6170e3
Violetta Doroshko <violet.doroshko@gmail.com>
2021-06-02 23:59:00 +0300

    refactor reader

--- src/main/java/epam/doroshko/array/reader/CustomArrayReader.java
+++ src/main/java/epam/doroshko/array/reader/CustomArrayReader.java
@@ -1,32 +1,7 @@
 package epam.doroshko.array.reader;
 
 import epam.doroshko.array.exception.CustomArrayException;
-import epam.doroshko.array.validator.CustomArrayValidator;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class CustomArrayReader {
-  private static final Logger logger = LogManager.getLogger();
-
-  public String readCustomArrayFromFile(String path) throws CustomArrayException, IOException {
-
-    try (var bufferedReader = new BufferedReader(new FileReader(path))) {
-      CustomArrayValidator validator = new CustomArrayValidator();
-      var string = bufferedReader.readLine();
-
-      while (string != null) {
-        if (validator.isValidate(string)) {
-          return string;
-        }
-        string = bufferedReader.readLine();
-      }
-      logger.log(Level.ERROR, "File is incorrect");
-      throw new CustomArrayException("File is incorrect");
-    }
-  }
+public interface CustomArrayReader {
+  String readCustomArrayFromFile(String path) throws CustomArrayException;
 }
---
+++ src/main/java/epam/doroshko/array/reader/impl/CustomArrayReaderImpl.java
@@ -0,0 +1,45 @@
+package epam.doroshko.array.reader.impl;
+
+import epam.doroshko.array.exception.CustomArrayException;
+import epam.doroshko.array.reader.CustomArrayReader;
+import epam.doroshko.array.validator.CustomArrayValidator;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CustomArrayReaderImpl implements CustomArrayReader {
+  private static final Logger logger = LogManager.getLogger("CustomArrayReader");
+
+  @Override
+  public String readCustomArrayFromFile(String path) throws CustomArrayException {
+
+    var resultString = "";
+
+    try {
+      var bufferedReader = new BufferedReader(new FileReader(path));
+      var string = bufferedReader.readLine();
+
+
+      while (string != null) {
+        if (CustomArrayValidator.validateInteger(string)) {
+          resultString = string;
+          break;
+        }
+        string = bufferedReader.readLine();
+      }
+      bufferedReader.close();
+
+    } catch (FileNotFoundException e) {
+      logger.log(Level.ERROR, "File can't be found");
+      throw new CustomArrayException("File can't be found");
+    } catch (IOException exception) {
+      logger.log(Level.ERROR, "File can't be open");
+      throw new CustomArrayException("File can't be open");
+    }
+    return resultString;
+  }
+}
